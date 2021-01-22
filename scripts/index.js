92131cf761339e09fcee6bd1ad2b6c22446feba5
Sergey Popov <sergeykms@gmail.com>
2021-01-22 23:37:09 +1000

    22_01_2021

--- scripts/index.js
+++ scripts/index.js
@@ -5,36 +5,61 @@ const clickOnImage = document.querySelector('.slider__image');
 const sliderImage = document.querySelectorAll('.slider__image');
 const backgroundImageSlide = document.querySelector('.slider');
 
+const sliderDots = document.querySelectorAll('.dots__item');
+
 
 let numberImage = 0;
+let numberDot = 0;
+
+function clearAllStyle() {
+   backgroundImageSlide.setAttribute("style", "background-image: url(../." + sliderImage[numberImage].getAttribute('src') + ")");
+   sliderImage[numberImage].classList.remove('slider__image_active');
+   sliderImage[numberImage].classList.remove('slider_move_left');
+   sliderImage[numberImage].classList.remove('slider_move_right');
+   sliderDots[numberDot].classList.remove('dots__item_active');
+}
+
+function moveSliderLeft() {
+   sliderImage[numberImage].classList.add('slider__image_active');
+   sliderImage[numberImage].classList.add('slider_move_left');
+   sliderDots[numberDot].classList.add('dots__item_active'); 
+}
+
+function moveSliderRight() {
+   sliderImage[numberImage].classList.add('slider_move_right');
+   sliderImage[numberImage].classList.add('slider__image_active');
+   sliderDots[numberDot].classList.add('dots__item_active');
+}
 
 function showNextImage() {
-  backgroundImageSlide.setAttribute("style", "background-image: url(../." + sliderImage[numberImage].getAttribute('src') + ")");
-  sliderImage[numberImage].classList.remove('slider__image_active');
-  if (numberImage == sliderImage.length - 1) {
-    numberImage = -1;
-  }
-  numberImage++
-  sliderImage[numberImage].classList.add('slider__image_active');
-  console.log(sliderImage[numberImage].getAttribute('src'));
+   clearAllStyle();
+   if (numberImage == sliderImage.length - 1) {
+      numberImage = -1;
+   }
+   numberImage++;
+   numberDot = numberImage
+   moveSliderLeft()
 }
 
 function showPrevImage() {
-  backgroundImageSlide.setAttribute("style", "background-image: url(../." + sliderImage[numberImage].getAttribute('src') + ")");
-  sliderImage[numberImage].classList.remove('slider__image_active');
-  if (numberImage == 0) {
-    numberImage = sliderImage.length;
-  }
-  numberImage--
-  sliderImage[numberImage].classList.add('slider__image_active');
+   clearAllStyle();
+   if (numberImage == 0) {
+      numberImage = sliderImage.length;
+   }
+   numberImage--;
+   numberDot = numberImage
+   moveSliderRight()
 }
 
 
 buttonNext.addEventListener('click', showNextImage);
-
 buttonPrev.addEventListener('click', showPrevImage);
 
-// sliderImage.forEach((item) => {
-//   item.addEventListener('click', showNextImage);
-//   });
-
+sliderDots.forEach((item, indexDot) => {
+	item.addEventListener('click', () => {
+      clearAllStyle();
+      numberDot = indexDot;
+      numberImage = numberDot
+      moveSliderLeft()
+	})
+});

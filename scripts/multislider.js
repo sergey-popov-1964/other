c47891a674fcd88a7bde5b34906ff5af9033e55e
Sergey Popov <sergeykms@gmail.com>
2021-02-03 16:43:33 +1000

    03_02_2021__1

--- scripts/multislider.js
+++ scripts/multislider.js
@@ -6,29 +6,38 @@ const multisliderImage = document.querySelectorAll('.multislider__img');
 
 let numberFirstMultisliderImage = 0;
 let numberEndMultisliderImage = 2;
-let currentMultisliderImage = 3;
+let currentMultisliderImage = 0;
+
+function clearNumber() {
+  for (i = currentMultisliderImage; i <= currentMultisliderImage + 2; i++) {
+    multisliderImage[i].classList.remove('multislider_move_left');
+    multisliderButtonNext.removeAttribute("disabled");
+  }
+}
 
 function showNextMultisliderImage() {
-
-  let currentImageWrapper = multisliderImageWrapper[numberFirstMultisliderImage]; // текущая первая обертка слайдера
-  let nextImageWrapper = multisliderImageWrapper[numberFirstMultisliderImage+1]; // следующая обертка слайдера
+  let currentImageWrapper = multisliderImageWrapper[currentMultisliderImage]; // текущая первая обертка слайдера
+  let nextImageWrapper = multisliderImageWrapper[currentMultisliderImage + 1]; // следующая обертка слайдера
 
   // устанавливаем у следующей обертки бекграундом изображение текущего первого слайда
   nextImageWrapper.setAttribute("style", "background-image: url(" + currentImageWrapper.querySelector('.multislider__img').getAttribute("src") + ")");
 
+  multisliderImageWrapper[currentMultisliderImage].classList.remove('multislider__img_active')
+  currentMultisliderImage++
+  multisliderImageWrapper[currentMultisliderImage + 2].classList.add('multislider__img_active')
 
-  multisliderImageWrapper[numberFirstMultisliderImage].classList.remove('multislider__img_active')
-  numberFirstMultisliderImage++
-  multisliderImageWrapper[numberFirstMultisliderImage + 2].classList.add('multislider__img_active')
-
-  for (let i = numberFirstMultisliderImage; i <= numberFirstMultisliderImage + 2; i++) {
+  for (let i = currentMultisliderImage; i <= currentMultisliderImage + 2; i++) {
     multisliderImage[i].classList.add('multislider_move_left');
   }
 
-  multisliderButtonNext.setAttribute("disabled", "disabled");
-  let timerId = setTimeout(clearNumber, 500)
+  if (currentMultisliderImage + 3 < multisliderImageWrapper.length) { // если не конец массива изображений продолжаем листать
+    multisliderButtonNext.setAttribute("disabled", "disabled");
+    let timerId = setTimeout(clearNumber, 500);
+  } else {
+    multisliderButtonNext.setAttribute("disabled", "disabled");
+    multisliderButtonNext.classList.add('multislider__btn_inactive');
+  }
 }
-
 
 function showPrevImageMultislider() {
   multisliderImageWrapper[0].classList.remove('multislider__img_active')
@@ -40,20 +49,3 @@ function showPrevImageMultislider() {
 
 multisliderButtonNext.addEventListener('click', showNextMultisliderImage);
 multisliderButtonPrev.addEventListener('click', showPrevImageMultislider);
-
-
-
-function clearNumber() {
-  for (i = numberFirstMultisliderImage; i <= numberFirstMultisliderImage + 2; i++) {
-    multisliderImage[i].classList.remove('multislider_move_left');
-    multisliderButtonNext.removeAttribute("disabled");
-  }
-}
-
-
-
-
-
-
-
-
